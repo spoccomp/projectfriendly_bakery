7e18e788d8938de664d2450431dedb34f7289147
richard trapanese <richardtrapanese@gmail.com>
2018-09-11 23:43:39 -0400

    updated all files

--- app_run.rb
+++ app_run.rb
@@ -6,21 +6,61 @@ include SendGrid
 
 get '/' do  
   
-  
+  @background ='
+  <style>
+  background-image: url("thumb-1920-2957.jpg") !Important;
+  background-position: center !Important;
+  background-repeat: no-repeat !Important; 
+  background-size: cover !Important;
+  </style>'
 erb (:home)
 end
+
 get '/home' do
+  @background ='
+  <style>
+  background-image: url("thumb-1920-2957.jpg");
+  background-position: center;
+  background-repeat: no-repeat; 
+  background-size: cover;
+  </style>'
+
   erb (:home)
 end
 get '/cookies' do
-  "Hello World"
+  @background ='
+  <style>
+  background-image: url("background_for_other_pages.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+    </style>'
+  erb (:cookies)
 end
 get '/cakes' do
-  "Hello World"
+  @background ='
+  <style>
+  background-image: url("background_for_other_pages.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+    </style>'
+  erb (:cakes)
 end
 get '/muffins' do
-  "Hello World"
+  @background ='
+  <style>
+  background-image: url("background_for_other_pages.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+    </style>'
+  erb (:muffins)
 end
-get '/cakes' do
-  "Hello World"
+get '/about' do
+
+  erb (:about)
+end
+get '/contact' do
+  erb (:contact)
 end
